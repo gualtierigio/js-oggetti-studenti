aee75083e14e059410958929c0a3545050db28c4
gualtierigio <gualtierigio@gmail.com>
2021-09-07 15:18:51 +0200

    problemi a ciclare all interno dell array con oggetti

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -8,6 +8,26 @@ let student = {
 
 for (let studentData in student){
     console.log(student[studentData]);
-}
+};
 
-let students = []
+let students = [ "",
+    student1 = {
+        name : "Baby",
+        surname : "Shark",
+        age : 32,
+    },
+    student2 = {
+        name : "Rosita",
+        surname : "Gallo",
+        age : 22,
+    },
+    student3 = {
+        name : "Pablo",
+        surname : "Escolab",
+        age : undefined,
+    }
+];
+
+for (let studentsData in students){
+    console.log(students[1][studentsData.name]);
+};
