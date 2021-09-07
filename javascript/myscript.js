3c74859c933175082a89874997af5f7a3e71af03
gualtierigio <gualtierigio@gmail.com>
2021-09-07 14:57:21 +0200

    first part complete

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,7 +1,13 @@
 
 
 let student = {
-    name : undefined,
-    surname : undefined,
-    age : undefined
+    name : "Gino",
+    surname : "Bello",
+    age : 31,
 };
+
+for (let studentData in student){
+    console.log(student[studentData]);
+}
+
+let students = []
